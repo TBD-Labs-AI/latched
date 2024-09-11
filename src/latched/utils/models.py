2d4411a4cc38ba09950370029dcf414db851285e
psycoder <parksang1993@gmail.com>
2024-09-11 19:16:12 +0900

    Add model size utility

---
+++ src/latched/utils/models.py
@@ -0,0 +1,30 @@
+# Copyright 2024 TBD Labs Inc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import torch.nn as nn
+
+
+def get_model_size(model: nn.Module, unit: str = "gb") -> float:
+    """Get the size of the model in MB or GB.
+
+    Args:
+        model (nn.Module): The model to get the size of.
+        unit (str): The unit to return the size in. Defaults to "gb".
+
+    Returns:
+        float: The size of the model in MB or GB.
+    """
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
+    size_all_mb = (param_size + buffer_size) / 1024**2
+    size_all_gb = size_all_mb / 1024
+    if unit == "mb":
+        return size_all_mb
+    elif unit == "gb":
+        return size_all_gb
+    else:
+        raise ValueError(f"Invalid unit: {unit}. Please specify 'mb' or 'gb'.")
