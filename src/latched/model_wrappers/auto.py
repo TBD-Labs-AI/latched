68574fc760ce81f92b8be6c3c437c38a6ee5e18b
sungmanch <sungman.cho@latched.ai>
2024-09-11 18:38:04 +0900

    Update the model wrapper

--- src/latched/model_wrappers/auto.py
+++ src/latched/model_wrappers/auto.py
@@ -8,6 +8,7 @@ from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 from transformers.models.phi3 import Phi3Model
 
 from latched.model_wrappers.base import BaseModelWrapper
+from latched.configs.device import DeviceConfig
 
 HuggingFaceTokenizer: TypeAlias = PreTrainedTokenizer | PreTrainedTokenizerFast
 
@@ -17,12 +18,20 @@ class AutoModelWrapper(BaseModelWrapper):
     AutoModelWrapper is a wrapper around the model from any library.
 
     Args:
-        model (BaseModelWrapper): The model wrapper from any library
+        model (nn.Module): The model from any library
+        precision (str, optional): The precision of the model. Defaults to "fp32".
+        device_config (DeviceConfig, optional): The device configuration. Defaults to None.
         tokenizer (AutoTokenizer, optional): The tokenizer from any library
     """
 
-    def __init__(self, model: nn.Module, tokenizer: HuggingFaceTokenizer | None = None):
-        super().__init__(model)
+    def __init__(
+        self,
+        model: nn.Module,
+        precision: str = "fp32",
+        device_config: DeviceConfig | None = None,
+        tokenizer: HuggingFaceTokenizer | None = None,
+    ):
+        super().__init__(model, precision, device_config)
         self.tokenizer = tokenizer
 
         # TODO: Remove this after designing the AutoModel for all the libraries
--- src/latched/model_wrappers/base.py
+++ src/latched/model_wrappers/base.py
@@ -1,11 +1,11 @@
 # Copyright 2024 TBD Labs Inc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-
 import torch.nn as nn
+from latched.configs.device import DeviceConfig
 
 
-class BaseModelWrapper(nn.Module):
+class BaseModelWrapper:
     """
     Base class for the model wrapper. Model wrapper converts any models from different libraries to a unified interface(nn.Module).
 
@@ -13,6 +13,12 @@ class BaseModelWrapper(nn.Module):
         model (nn.Module): The model wrapper for handling the model from any library
     """
 
-    def __init__(self, model: nn.Module):
-        super().__init__()
+    def __init__(
+        self,
+        model: nn.Module,
+        precision: str = "fp32",
+        device_config: DeviceConfig | None = None,
+    ):
         self.model = model
+        self.precision = precision
+        self.device_config = device_config
