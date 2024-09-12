83ccdf58eb627df0c2257e4059e504d156ba31b4
psycoder <parksang1993@gmail.com>
2024-09-12 21:28:04 +0900

    Use file system to get model size

--- src/latched/utils/__init__.py
+++ src/latched/utils/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2024 TBD Labs Inc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from .models import get_model_size
+from .models import get_size_of_model
 from .profile import profile, profile_decorator
 
-__all__ = ["get_model_size", "profile", "profile_decorator"]
+__all__ = ["get_size_of_model", "profile", "profile_decorator"]
