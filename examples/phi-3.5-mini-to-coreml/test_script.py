c4b4a449fc7b4fd3314faaade810f46f64700dc3
SangUk Park <psycoder@latched.ai>
2024-09-18 15:21:46 +0900

    update example files

--- examples/phi-3.5-mini-to-coreml/test_script.py
+++ examples/phi-3.5-mini-to-coreml/test_script.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import TypeAlias
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -7,11 +8,14 @@ from latched.utils import profile, profile_decorator
 
 model_path = "microsoft/Phi-3.5-mini-instruct"
 
+TracedModule: TypeAlias = torch.jit._trace.TopLevelTracedModule  # type: ignore
+
 
 @profile_decorator
 def prepare_model():
     model = AutoModelForCausalLM.from_pretrained(model_path, torchscript=True)
     model.eval()
+
     return model
 
 
@@ -22,8 +26,9 @@ def prepare_tokenizer():
 
 
 @profile_decorator
-def jit_trace_model(model, input_tokens):
+def jit_trace_model(model, input_tokens) -> TracedModule:
     traced_model = torch.jit.trace(model, input_tokens)
+    print(type(traced_model))
     return traced_model
 
 
@@ -67,6 +72,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--script", action="store_true")
     parser.add_argument("--trace", action="store_true")
+    parser.add_argument("--acc", type=str, choices=["cpu", "mps", "cuda"], default="cpu")
     return parser.parse_args()
 
 
@@ -74,6 +80,7 @@ if __name__ == "__main__":
     import os
 
     args = parse_args()
+    device = torch.device(args.acc)
     tokenizer = prepare_tokenizer()
     text_input = tokenizer.encode("Hello, how are you?", return_tensors="pt")
     sample_input = torch.randint(1000, (1, 10))
@@ -82,8 +89,10 @@ if __name__ == "__main__":
         print("Tracing model...")
         model = prepare_model()
         model.eval()
+
+        model.to(device)
         with profile("jit_trace_model"):
-            traced_model = jit_trace_model(model, sample_input)
+            traced_model = jit_trace_model(model, sample_input.to(device))
         torch.jit.save(traced_model, f"{model_path.replace('/', '_')}_traced_model.pt")
     else:
         print("Loading traced model...")
@@ -92,29 +101,30 @@ if __name__ == "__main__":
         else:
             raise ValueError("Traced model not found")
 
-    if args.script:
-        print("Scripting model...")
-        wrapped_model = LLMModelWrapper(traced_model, tokenizer.eos_token_id)
-        with profile("torch.jit.script"):
-            scripted_model = torch.jit.script(wrapped_model)
-        scripted_model.save(f"{model_path.replace('/', '_')}_scripted_model.pt")
-    else:
-        print("Loading scripted model...")
-        if os.path.exists(f"{model_path.replace('/', '_')}_scripted_model.pt"):
-            scripted_model = torch.jit.load(f"{model_path.replace('/', '_')}_scripted_model.pt")
-        else:
-            raise ValueError("Scripted model not found")
+    # if args.script:
+    #     print("Scripting model...")
+    #     wrapped_model = LLMScriptModelWrapper(traced_model, eos_token_id=tokenizer.eos_token_id)
+    #     with profile("torch.jit.script"):
+    #         scripted_model = torch.jit.script(wrapped_model)
+    #     scripted_model.save(f"{model_path.replace('/', '_')}_scripted_model.pt")
+    # else:
+    #     print("Loading scripted model...")
+    #     if os.path.exists(f"{model_path.replace('/', '_')}_scripted_model.pt"):
+    #         scripted_model = torch.jit.load(f"{model_path.replace('/', '_')}_scripted_model.pt")
+    #     else:
+    #         raise ValueError("Scripted model not found")
 
     import coremltools as ct
     import numpy as np
 
-    scripted_model = scripted_model.eval()
-    scripted_model = scripted_model.to("cpu")
+    traced_model = traced_model.eval()
+    traced_model = traced_model.to("cpu")
     with profile("coremltools.convert"):
+        ct_shape = ct.Shape(shape=(1, ct.RangeDim(1, 1000)))
         mlmodel = ct.convert(
-            scripted_model,
+            traced_model,
             convert_to="mlprogram",
-            inputs=[ct.TensorType(shape=sample_input.shape, dtype=np.int32)],
+            inputs=[ct.TensorType(shape=ct_shape, dtype=np.int32)],
             source="pytorch",
         )
 
